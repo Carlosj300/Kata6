d3f497ae3439310bf3b9019a72f200af35a70694
Usuario <Usuario@192.168.0.164>
2019-01-18 22:56:48 +0000

    Primera version, lee archivos desde cierto root mide su tamaño y muestra un histograma con la cantidad de archivos por tamaño

---
+++ src/kata6/Kata6.java
@@ -0,0 +1,42 @@
+package kata6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @author Carlos
+ */
+public class Kata6 {
+    Map<Integer, Integer> map;
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        Kata6 kata6 = new Kata6();
+        kata6.execute();
+        
+    }
+
+    private void execute() {
+        input();
+        process();
+        output(); 
+    }
+
+    private void input() {
+        MapCreator mapCreator = new MapCreator();
+        map = mapCreator.getMegabytesMap(); 
+        
+    }
+
+    private void process() {
+        //No process required for this version
+    }
+
+    private void output() {
+        HistogramDisplay histoDisplay = new HistogramDisplay(map);
+        histoDisplay.execute();
+    }
+    
+}
